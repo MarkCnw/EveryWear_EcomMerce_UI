db9e0427ae1fff9fff5e08ea8d5df7cc75cbf9fa
MarkCnw <chinnawong554@gmail.com>
2025-01-10 23:19:17 +0700

    Add shopping bag icon with notification badge to home screen and update asset paths

--- ecommerce_app_ui/lib/Views/app_home_screen.dart
+++ ecommerce_app_ui/lib/Views/app_home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
 
 class AppHomeScreen extends StatefulWidget {
   const AppHomeScreen({super.key});
@@ -10,8 +11,53 @@ class AppHomeScreen extends StatefulWidget {
 class _AppHomeScreenState extends State<AppHomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            const SizedBox(height: 40,),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+              Image.asset("assets/logo7.png",
+              height: 80,
+              ),
+              Stack(
+                clipBehavior: Clip.none,
+                children: [
+                  const Icon(
+                    Iconsax.shopping_bag,
+                    size: 28,
+                  ),
+                  Positioned(
+                    right: -3,top: -5,
+                    child: Container(
+                    padding: const EdgeInsets.all(4),
+                    decoration: const BoxDecoration(
+                      color: Colors.red,
+                      shape: BoxShape.circle,
+                    ),
+                    child: const Center(
+                      child: Text(
+                        "3",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ),
+                  ),
+                ],
+              )
+            ],),
+            ),
+        ],
+        ),
+      ),
     );
   }
 }
